88fc6e7ffbdfb930b51da0efa446b69a6e977e7c
victobes <57673028+victobes@users.noreply.github.com>
2024-11-05 00:12:06 +0300

    add pwa

--- book-office-app/vite.config.js
+++ book-office-app/vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'Book Publishing Company',
+                short_name: 'BPC',
+                description: 'Book printing and publishing services',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     base: "/book-office-web-app-frontend/",
     server: {
         port: 3000,

3fb38524096da8336db061031014778056e3bc18
victobes <57673028+victobes@users.noreply.github.com>
2024-11-06 02:22:34 +0300

    add localhost tauri

--- book-office-app/src/App.tsx
+++ book-office-app/src/App.tsx
@@ -4,8 +4,22 @@ import { AppRoutes } from "./Routes"
 import { useGlobalProps } from "./hooks/useGlobalProps"
 import { Provider } from "react-redux";
 import { store } from './core/store';
+import {useEffect} from "react";
+
+const {invoke} = (window as any).__TAURI__.tauri;
 
 function App() {
+  useEffect(() => {
+    invoke('tauri', {cmd: 'create'})
+        .then((response: any) => console.log(response))
+        .catch((error: any) => console.log(error));
+    return () => {
+        invoke('tauri', {cmd: 'close'})
+            .then((response: any) => console.log(response))
+            .catch((error: any) => console.log(error));
+    }
+}, []);
+
   const globalProps = useGlobalProps();
 
   return (
